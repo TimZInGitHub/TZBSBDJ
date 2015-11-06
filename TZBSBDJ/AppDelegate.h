61e52fd9e2b027ccfce86537aa561b160b0cbd57
TimZInGitHub <Tim@11234.local>
2015-11-06 12:46:08 +0800

    Initial Commit

---
+++ TZBSBDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TZBSBDJ
+//
+//  Created by Tim.Z on 15/11/6.
+//  Copyright © 2015年 Tim.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TZBSBDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TZBSBDJ
+//
+//  Created by Tim.Z on 15/11/6.
+//  Copyright © 2015年 Tim.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
